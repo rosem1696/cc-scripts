c61a7ee1512d7728662c5842a21c224dadc83d72
Mitchell Rose <rosem1696@gmail.com>
2023-03-31 17:18:15 -0500

    list is a function

--- lib/inventory/inventory.lua
+++ lib/inventory/inventory.lua
@@ -1,6 +1,6 @@
 local function hasOpenSlot(inv)
     local count = 0
-    for slot, item in pairs(inv.list) do
+    for slot, item in pairs(inv.list()) do
         if item.count > 0 then count = count + 1 end
     end
 
